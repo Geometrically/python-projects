bd3e79898f3b4b51d1a7bc148c9155d34f758981
Geometrically <jaiagr123@gmail.com>
2019-11-15 15:41:02 -0700

    Render X's and O's

--- tic-tac-toe/tictac.py
+++ tic-tac-toe/tictac.py
@@ -2,6 +2,8 @@ import turtle
 import math
 
 def draw_line(x1, y1, x2, y2):
+    pointer.color("red")
+
     pointer.up()
     pointer.goto(x1, y1)
     pointer.down()
@@ -11,36 +13,61 @@ def draw_line(x1, y1, x2, y2):
 def check_win():
     global squares
     
+    win = False
+    
     for row in squares:
-        if len(set(row)) <= 1:
-            print("win")
-            return True
-
+        for box in row:
+            if box == 0:
+                win = False
+                break
+            win = True
+        if win:
+            win = row.count(row[0]) == len(row)
+            
+            if win:
+                break
+    
+    for column in zip(*matrix): 
+    
+    print(win)
+    
 def box_clicker(x, y):
     global player1Turn
     global squares
         
     if (x % 1 > 0.9 or x % 1 < 0.1) or (y % 1 > 0.9 or y % 1 < 0.1):
         return
-    
+        
     squareX = int(math.floor(x))
     squareY = int(math.floor(y))
     
     square = squares[squareX][squareY]
         
-    if(square == 0):
-        pointer.goto(squareX + 0.5, squareY + 0.5)
+    if(square != 0):
+        return
+    
+    pointer.goto(squareX + 0.5, squareY + 0.5)
     
     if player1Turn:
-        pointer.shape("circle")
+        draw_line(squareX + 0.25, squareY + 0.75, squareX + 0.75, squareY + 0.25)
+        draw_line(squareX + 0.25, squareY + 0.25, squareX + 0.75, squareY + 0.75)
+
         player1Turn = False
-        square = 1
+        squares[squareX][squareY] = 1
     else:
-        pointer.shape("turtle")
+        pointer.shape("circle")
+        
+        pointer.shapesize(7)
+        pointer.color("red")
+        pointer.stamp()
+        
+        pointer.shapesize(5)
+        pointer.color("white")
+        pointer.stamp()
+                
         player1Turn = True
-        square = 2
-    
-    pointer.stamp()
+        squares[squareX][squareY] = 2
+
     check_win()
     
 def clear():
@@ -55,12 +82,10 @@ screen.screensize(700,700)
 screen.setworldcoordinates(0, 0, 3, 3)
 
 pointer = turtle.Turtle()
-pointer.shape("turtle")
 pointer.hideturtle()
 pointer.up()
 pointer.color("red")
 pointer.speed(0)
-pointer.shapesize(3, 3)
 pointer.pensize(15)
 
 draw_line(1, -1, 1, 3)
